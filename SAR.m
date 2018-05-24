56c0b8778f8dfce9dfd7fa1aae60ca29215defe5
Lucanoel <luck3596@hotmail.be>
2018-05-24 23:28:41 +0200

    Add files via upload

--- SAR.m
+++ SAR.m
@@ -1,5 +1,5 @@
 % PROJECT COMMUNICATION CHANNEL
-clear all;
+close all;
 clc;
 frequency_ref=9e+9;
 frequency=9e+8;
@@ -26,13 +26,13 @@ refl_coeff=1e-6;
 sigma_e=[];
 acum=0;
 
-
 size_sigma_vec=6;
 
 for i=1:size_sigma_vec
     acum=acum+1/size_sigma_vec;
     sigma_e(i)=acum*1e-1/8;
 end
+
 sigma_m=mu0*sigma_e/epsilon0;
 Eps_mat=ones(xdim,ydim);
 mu_mat=ones(xdim,ydim);
@@ -68,24 +68,21 @@ for i=1:length(sigma_e)
     sigma(:,i)=sigma_e(i);
     sigma(:,ydim-size_sigma_vec-1+i)=sigma_e(i);
 end
+
 for i=1:length(sigma_m)
     sigmam(i,:)=sigma_m(i);
     sigmam(xdim-size_sigma_vec-1+i:xdim-1,:)=sigma_m(i);
     sigmam(:,i)=sigma_m(i);
     sigmam(:,ydim-size_sigma_vec-1+i)=sigma_m(i);
 end
+
 Eps=epsilon0*((Eps_mat));
 Mu=mu0*(mu_mat);
 % Initialization of field matrices
 Ez=zeros(xdim,ydim);
-%Ezx=zeros(xdim,ydim);
-%Ezy=zeros(xdim,ydim);
 Hy=zeros(xdim,ydim);
 Hx=zeros(xdim,ydim);
 Power=zeros(xdim,ydim);
-rayons=zeros(xdim,ydim);
-log10ez=zeros(xdim,ydim);
-
 
 %Multiplication factor matrices for E matrix update to avoid being calculated many times 
 %in the time update loop so as to increase computation speed                           
@@ -96,7 +93,6 @@ cst5=(1-sigmam*deltat./(2*Mu))./(1+sigmam*deltat./(2*Mu));
 % Update loop begins
 for n=1:1:time_tot
     
-        % Setting time dependent boundaries to update only relevant parts of the 
     % matrix where the wave has reached to avoid unnecessary updates.
     if n<xsource-2
         n1=xsource-n-1;
@@ -118,17 +114,15 @@ for n=1:1:time_tot
         n21=ysource+n;
     else
         n21=ydim-1;
-    end
-    
+    end    
     %matrix update instead of for-loop for Ez field  
-
     
     Hx(n1:n2,n11:n21)=cst5(n1:n2,n11:n21).*Hx(n1:n2,n11:n21)-cst2(n1:n2,n11:n21).*(Ez(n1:n2,n11+1:n21+1)-Ez(n1:n2,n11:n21));
     Hy(n1:n2,n11:n21)=cst5(n1:n2,n11:n21).*Hy(n1:n2,n11:n21)+cst2(n1:n2,n11:n21).*(Ez(n1+1:n2+1,n11:n21)-Ez(n1:n2,n11:n21));
     
     Ez(n1+1:n2+1,n11+1:n21+1)=cst4(n1+1:n2+1,n11+1:n21+1).*Ez(n1+1:n2+1,n11+1:n21+1)+cst(n1+1:n2+1,n11+1:n21+1).*(-Hx(n1+1:n2+1,n11+1:n21+1)+Hx(n1+1:n2+1,n11:n21))+cst(n1+1:n2+1,n11+1:n21+1).*(Hy(n1+1:n2+1,n11+1:n21+1)-Hy(n1:n2,n11+1:n21+1));
     Power(n1+1:n2+1,n11+1:n21+1)=(Ez(n1+1:n2+1,n11+1:n21+1).^2/(2*120*pi));
-    SAR=sigma.*(Ez.^2)./Rho;
+    sar=sigma.*(Ez.^2)./Rho;
     
     if n11==1
         Ez(:,n11)=0;
@@ -156,8 +150,7 @@ for n=1:1:time_tot
 %     surf(delta*1e+6*(1:1:xdim),(delta*1e+6*(1:1:ydim)),Ez);
 %     hold on;
     frac = 1/3;
-
-    imagesc((round(frac*xdim):round((1-frac)*xdim))*delta,((round(frac*ydim):round((1-frac)*ydim))*delta),10*log10(abs(SAR((round(frac*xdim):round((1-frac)*xdim)),(round(frac*ydim):round((1-frac)*ydim))))),[-120 0]);colorbar; %zoomed
+    imagesc((round(frac*xdim):round((1-frac)*xdim))*delta,((round(frac*ydim):round((1-frac)*ydim))*delta),10*log10(abs(sar((round(frac*xdim):round((1-frac)*xdim)),(round(frac*ydim):round((1-frac)*ydim))))),[-120 0]);colorbar; %zoomed
     colormap(jet);
     hold on
     theta = linspace(0,2*pi,100);
